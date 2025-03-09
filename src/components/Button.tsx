7b9e9e35a4a177eae667216c1cc45184262c407d
Jose Francisco Ruso Alvarez <jrusoalv@emeal.nttdata.com>
2025-03-09 15:29:16 +0100

    Add Button component with multiple variants and styling

---
+++ src/components/Button.tsx
@@ -0,0 +1,78 @@
+import { ReactNode } from "react";
+
+export type ButtonType = "basic" | "link" | "success" | "plain";
+
+const baseClasses = [
+  "inline-flex",
+  "items-center",
+  "justify-center",
+  "rounded",
+  "font-semibold",
+  "focus:outline-none",
+  "focus:ring-2",
+  "transition",
+  "duration-200",
+  "ease-in-out",
+  "px-6",
+  "py-2",
+  "text-center",
+  "leading-none",
+];
+
+const basicClasses = [
+  "border-2",
+  "border-blue-600",
+  "bg-blue-600",
+  "text-white",
+  "hover:bg-blue-700",
+  "focus:ring-blue-400",
+];
+
+const linkClasses = [
+  "border-2",
+  "border-blue-600",
+  "bg-white",
+  "text-blue-600",
+  "hover:underline",
+  "hover:text-blue-700",
+  "focus:ring-blue-400",
+];
+
+const successClasses = [
+  "bg-green-600",
+  "text-white",
+  "hover:bg-green-700",
+  "focus:ring-green-400",
+];
+
+const plainClasses = ["border-none", "bg-transparent", "text-black"];
+
+export function Button({
+  onClick,
+  children,
+  variant = "basic",
+}: {
+  variant?: ButtonType;
+  children?: ReactNode;
+  onClick: () => void;
+}) {
+  const handleButtonTypeClass = (type: ButtonType) => {
+    const classes = {
+      basic: basicClasses,
+      link: linkClasses,
+      success: successClasses,
+      plain: plainClasses,
+    };
+
+    return [...baseClasses, ...classes[type]].join(" ");
+  };
+
+  return (
+    <button
+      onClick={() => onClick()}
+      className={handleButtonTypeClass(variant)}
+    >
+      {children}
+    </button>
+  );
+}
